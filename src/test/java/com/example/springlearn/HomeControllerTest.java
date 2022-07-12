2cd4ca20db27a541dea24ffcf157443ff89e2492
kivanval <kivanval@gmail.com>
2022-07-13 02:57:12 +0300

    Updated Controllers

--- src/test/java/com/example/springlearn/HomeControllerTest.java
+++ src/test/java/com/example/springlearn/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.springlearn;
 
-import com.example.springlearn.web.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
